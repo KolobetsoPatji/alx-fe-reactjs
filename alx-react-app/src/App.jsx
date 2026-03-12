48731ebe7b45cca5c70688ecbe8cc562d84c61d4
Kolobetso Patji <eunivor@gmail.com>
2026-03-13 08:46:28 +1400

    Add UserProfile component and usage in App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,7 +1,8 @@
 import WelcomeMessage from './components/WelcomeMessage'; 
-import Header from './Header';
-import MainContent from './MainContent';
-import Footer from './Footer';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
 
 function App() {
   return (
@@ -10,6 +11,18 @@ function App() {
       <Header />
       <MainContent />
       <Footer />
+
+      {/* Example usage of UserProfile */}
+      <UserProfile
+        name= "Alice"
+        age={25}
+        bio="Loves hiking and photography"
+        />
+        <UserProfile
+          name="BOb"
+          age={30}
+          bio="Enjoys cooking and traveling"
+          />
     </div>
   );
 }
