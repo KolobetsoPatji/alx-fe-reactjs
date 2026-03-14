4afb8c9664dabe0c12269b8597f80973ce5092fb
Kolobetso Patji <eunivor@gmail.com>
2026-03-15 10:33:43 +1400

    Build simple company website with React Router

---
+++ my-company/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <footer style={{ backgroundColor: 'black', color: 'white', textAlign: 'center', padding: '10px', marginTop: '20px' }}>
+            <p>Copyright 2026 My Company</p>
+        </footer> 
+    );
+}
+
+export default Footer;
