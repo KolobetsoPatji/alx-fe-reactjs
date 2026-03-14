3d19d84237807596b23db21e278a52e3c278542f
Kolobetso Patji <eunivor@gmail.com>
2026-03-15 07:16:21 +1400

    fix: inline styling with alx-react-app-new

--- alx-react-app-new/src/components/Footer.jsx
+++ alx-react-app-new/src/components/Footer.jsx
@@ -1,7 +1,9 @@
 function Footer() {
     return(
-        <footer style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center' }}>
-            <p>Copyright 2023 City Lovers</p>
+        <footer style={{ backgroundColor: 'black', color: 'white', textAlign: 'center', padding: '15px', marginTop:'20px' }}>
+            <p style={{ fontSize: '14px' }}>
+                Copyright 2023 City Lovers
+        </p>
         </footer>
     );
 }
--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,7 +1,8 @@
 function MainContent() {
     return(
-        <main style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center' }}>
-            <p>I love to visit New York, Paris, and Tokyo.</p>
+        <main style={{ padding: '20px', backgroundColor: '#f0f0f0', textAlign: 'center' }}>
+            <p style={{ fontSize: '18px', color: '#333', margin: '10px' }}>
+              I love to visit New York, Paris, and Tokyo.</p>
         </main>
     );
 }
